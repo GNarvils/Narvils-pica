30d83b43084aac700e75881f2b9c0cd07280d090
Narvils <gustavs.gn@gmail.com>
2022-01-26 09:05:56 +0200

    Main klase sakums

--- src/pica/Picerija.java
+++ src/pica/Picerija.java
@@ -1,9 +1,11 @@
 package pica;
 
+import javax.swing.JOptionPane;
+
 public class Picerija {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		JOptionPane.showMessageDialog(null, "Esiet sveicināti picērijā");
 
 	}
 
